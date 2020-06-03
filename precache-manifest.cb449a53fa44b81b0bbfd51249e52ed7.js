b94411ebe7f3fec5e2d2408cda76812afb2d545c
jaykalia <65418911+jaykalia@users.noreply.github.com>
2020-06-04 02:09:55 +0530

    Updates

---
+++ precache-manifest.cb449a53fa44b81b0bbfd51249e52ed7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac2540ae8ca3097338cbf3f67edb1bd3",
+    "url": "/meme-generator/index.html"
+  },
+  {
+    "revision": "cf46d1a24d21d056eb2c",
+    "url": "/meme-generator/static/css/main.6d4dbdaa.chunk.css"
+  },
+  {
+    "revision": "13b1237dbaeb5b413912",
+    "url": "/meme-generator/static/js/2.070775ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/meme-generator/static/js/2.070775ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf46d1a24d21d056eb2c",
+    "url": "/meme-generator/static/js/main.67407f5e.chunk.js"
+  },
+  {
+    "revision": "d61cbcd042e901af7906",
+    "url": "/meme-generator/static/js/runtime-main.df969e4a.js"
+  }
+]);
